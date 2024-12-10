bfb03b7252be5b2651a3ed4b6b1a352f047268a3
jimmylizheng <jimmyli@umich.edu>
2024-12-10 14:40:57 -0500

    update plotting script

--- bar_plot.py
+++ bar_plot.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 
 # Sample data
 categories = ['Original', 'Size', 'Opacity', 'Brightness']
-# values_set1 = [10, 15, 7, 12]
-# values_set2 = [8, 12, 10, 9]
 # 294.68MB, b1=29.47MB/s, b2=9.82MB/s
 b1_SSIM_set=[0.6364, 0.6937, 0.6025, 0.6322] # for avg SSIM
 b2_SSIM_set=[0.6046, 0.6098, 0.6376, 0.5450]
@@ -27,8 +25,8 @@ positions2 = [p + bar_width for p in positions1]
 plt.figure(figsize=(10, 6))
 
 # Create the bar plots
-plt.bar(positions1, b1_SSIM_set, width=bar_width, color='skyblue', label='29.47MB/s', edgecolor='black', linewidth=1.5)
-plt.bar(positions2, b2_SSIM_set, width=bar_width, color='orange', label='9.82MB/s', edgecolor='black', linewidth=1.5)
+# plt.bar(positions1, b1_SSIM_set, width=bar_width, color='skyblue', label='29.47MB/s', edgecolor='black', linewidth=1.5)
+# plt.bar(positions2, b2_SSIM_set, width=bar_width, color='orange', label='9.82MB/s', edgecolor='black', linewidth=1.5)
 
 # plt.bar(positions1, b1_qoe_SSIM_set, width=bar_width, color='skyblue', label='29.47MB/s', edgecolor='black', linewidth=1.5)
 # plt.bar(positions2, b2_qoe_SSIM_set, width=bar_width, color='orange', label='9.82MB/s', edgecolor='black', linewidth=1.5)
@@ -36,12 +34,12 @@ plt.bar(positions2, b2_SSIM_set, width=bar_width, color='orange', label='9.82MB/
 # plt.bar(positions1, b1_PSNR_set, width=bar_width, color='skyblue', label='29.47MB/s', edgecolor='black', linewidth=1.5)
 # plt.bar(positions2, b2_PSNR_set, width=bar_width, color='orange', label='9.82MB/s', edgecolor='black', linewidth=1.5)
 
-# plt.bar(positions1, b1_qoe_PSNR_set, width=bar_width, color='skyblue', label='29.47MB/s', edgecolor='black', linewidth=1.5)
-# plt.bar(positions2, b2_qoe_PSNR_set, width=bar_width, color='orange', label='9.82MB/s', edgecolor='black', linewidth=1.5)
+plt.bar(positions1, b1_qoe_PSNR_set, width=bar_width, color='skyblue', label='29.47MB/s', edgecolor='black', linewidth=1.5)
+plt.bar(positions2, b2_qoe_PSNR_set, width=bar_width, color='orange', label='9.82MB/s', edgecolor='black', linewidth=1.5)
 
 # Add a dashed baseline for avg SSIM
-plt.axhline(y=0.5031, color='black', linestyle='--', linewidth=1.5, label='Baseline (29.47MB/s)')
-plt.axhline(y=0.3600, color='black', linestyle='-.', linewidth=1.5, label='Baseline (9.82MB/s)')
+# plt.axhline(y=0.5031, color='black', linestyle='--', linewidth=1.5, label='Baseline (29.47MB/s)')
+# plt.axhline(y=0.3600, color='black', linestyle='-.', linewidth=1.5, label='Baseline (9.82MB/s)')
 # Add a dashed baseline for qoe SSIM
 # plt.axhline(y=0.5844, color='black', linestyle='--', linewidth=1.5, label='Baseline (29.47MB/s)')
 # plt.axhline(y=0.3879, color='black', linestyle='-.', linewidth=1.5, label='Baseline (9.82MB/s)')
@@ -49,29 +47,27 @@ plt.axhline(y=0.3600, color='black', linestyle='-.', linewidth=1.5, label='Basel
 # plt.axhline(y=20.0445, color='black', linestyle='--', linewidth=1.5, label='Baseline (29.47MB/s)')
 # plt.axhline(y=15.5349, color='black', linestyle='-.', linewidth=1.5, label='Baseline (9.82MB/s)')
 # Add a dashed baseline for qoe PSNR
-# plt.axhline(y=28.0425, color='black', linestyle='--', linewidth=1.5, label='Baseline (29.47MB/s)')
-# plt.axhline(y=22.0618, color='black', linestyle='-.', linewidth=1.5, label='Baseline (9.82MB/s)')
+plt.axhline(y=28.0425, color='black', linestyle='--', linewidth=1.5, label='Baseline (29.47MB/s)')
+plt.axhline(y=22.0618, color='black', linestyle='-.', linewidth=1.5, label='Baseline (9.82MB/s)')
 
 # Add titles and labels
-plt.title('Measurement Result of Average SSIM', fontsize=24)
+# plt.title('Measurement Result of Average SSIM', fontsize=24)
 # plt.title('Measurement Result of QoE(SSIM)', fontsize=24)
 # plt.title('Measurement Result of Average PSNR', fontsize=24)
 # plt.title('Measurement Result of QoE(PSNR)', fontsize=24)
 plt.xlabel('Utility', fontsize=22)
-plt.ylabel('SSIM', fontsize=22)
+# plt.ylabel('SSIM', fontsize=22)
 # plt.ylabel('PSBR(dB)', fontsize=22)
 # plt.ylabel('QoE(SSIM)($s^{-1}$)', fontsize=22)
-# plt.ylabel('QoE(PSBR)(dB/s)', fontsize=22)
+plt.ylabel('QoE(PSBR)(dB/s)', fontsize=22)
 plt.xticks([p + bar_width / 2 for p in positions1], categories, fontsize=20)  # Centering category labels
 plt.yticks(fontsize=20)
 plt.legend(fontsize=16, framealpha=1,loc='lower left')  # Add legend
 
-# plt.ylim(0.5,0.8)
-
 # Save the plot as an image
-plt.savefig('./result/avg_ssim_bar_plot.png', dpi=300, bbox_inches='tight')  # Adjust DPI and bounding box if needed
+# plt.savefig('./result/avg_ssim_bar_plot.png', dpi=300, bbox_inches='tight')  # Adjust DPI and bounding box if needed
 # plt.savefig('./result/qoe_ssim_bar_plot.png', dpi=300, bbox_inches='tight')  # Adjust DPI and bounding box if needed
 # plt.savefig('./result/avg_psnr_bar_plot.png', dpi=300, bbox_inches='tight')  # Adjust DPI and bounding box if needed
-# plt.savefig('./result/qoe_psnr_bar_plot.png', dpi=300, bbox_inches='tight')  # Adjust DPI and bounding box if needed
+plt.savefig('./result/qoe_psnr_bar_plot.png', dpi=300, bbox_inches='tight')  # Adjust DPI and bounding box if needed
 
 plt.close()  # Close the figure to free memory
--- dist_plot.py
+++ dist_plot.py
@@ -10,10 +10,14 @@ with open('gaussian_data.json', 'r') as f:
     data = json.load(f)
 
 # Convert lists to numpy arrays for easier handling
-positions = np.array(data['positions']).reshape(-1, 3)
+# positions = np.array(data['positions']).reshape(-1, 3)
+color_sum = np.array(data['color_sum'])
+brightness = np.array(data['brightness'])
+scale_mul = np.array(data['scale_mul'])
+scale_op_mul = np.array(data['scale_op_mul'])
 opacities = np.array(data['opacities'])
-colors = np.array(data['colors']).reshape(-1, 3)
-cov3Ds = np.array(data['cov3Ds']).reshape(-1, 6)
+# colors = np.array(data['colors']).reshape(-1, 3)
+# cov3Ds = np.array(data['cov3Ds']).reshape(-1, 6)
 
 # Function to save plot
 def save_plot(fig, filename):
@@ -26,42 +30,52 @@ def calculate_cdf(data):
     cdf = np.arange(1, len(sorted_data)+1) / len(sorted_data)
     return sorted_data, cdf
 
-# 1. Plot and save the CDF of positions (X, Y, Z coordinates)
-# fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+# fig = plt.figure(figsize=(8, 6))
+# sorted_color_sum, cdf_color_sum = calculate_cdf(color_sum)
+# plt.plot(sorted_color_sum, cdf_color_sum, color='purple')
+# # plt.title('CDF of color_sum')
+# plt.xticks(fontsize=20)
+# plt.yticks(fontsize=20)
+# plt.xlabel('color_sum', fontsize=22)
+# save_plot(fig, 'result/color_sum_cdf.png')
 
-# for i, label, color in zip(range(3), ['X Position', 'Y Position', 'Z Position'], ['r', 'g', 'b']):
-#     sorted_data, cdf = calculate_cdf(positions[:, i])
-#     axes[i].plot(sorted_data, cdf, color=color, label=label)
-#     axes[i].legend()
+fig = plt.figure(figsize=(9, 8))
+sorted_brightness, cdf_brightness = calculate_cdf(brightness)
+plt.plot(sorted_brightness, cdf_brightness, color='blue', linewidth=2)
+# plt.title('CDF of brightness')
+plt.xlabel('brightness', fontsize=22)
+plt.xlim(0, 1.75)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+save_plot(fig, 'result/brightness_cdf.png')
 
-# fig.suptitle('CDF of Positions')
-# save_plot(fig, 'positions_cdf.png')
+fig = plt.figure(figsize=(9, 8))
+sorted_scale_mul, cdf_scale_mul = calculate_cdf(scale_mul)
+plt.plot(sorted_scale_mul, cdf_scale_mul, color='blue', linewidth=2)
+# plt.title('CDF of scale_mul')
+plt.xlabel('Scale', fontsize=22)
+# plt.xlim(0, 0.01)
+plt.xlim(1e-10, 1)
+plt.xscale('log')
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+save_plot(fig, 'result/scale_mul_cdf.png')
 
-# 2. Plot and save the CDF of opacities
-fig = plt.figure(figsize=(8, 6))
+# fig = plt.figure(figsize=(8, 6))
+# sorted_scale_op_mul, cdf_scale_op_mul = calculate_cdf(scale_op_mul)
+# plt.plot(sorted_scale_op_mul, cdf_scale_op_mul, color='purple')
+# # plt.title('CDF of scale_op_mul')
+# plt.xlabel('scale_op_mul')
+# # plt.xlim(0, 0.001)
+# plt.xlim(1e-10, 1)
+# plt.xscale('log')
+# save_plot(fig, 'result/scale_op_mul_cdf.png')
+
+fig = plt.figure(figsize=(9, 8))
 sorted_opacities, cdf_opacities = calculate_cdf(opacities)
-plt.plot(sorted_opacities, cdf_opacities, color='purple')
-plt.title('CDF of Opacities')
-plt.xlabel('Opacity')
-save_plot(fig, 'opacities_cdf.png')
-
-# 3. Plot and save the CDF of colors (R, G, B channels)
-# fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-
-# for i, label, color in zip(range(3), ['Red Channel', 'Green Channel', 'Blue Channel'], ['r', 'g', 'b']):
-#     sorted_data, cdf = calculate_cdf(colors[:, i])
-#     axes[i].plot(sorted_data, cdf, color=color, label=label)
-#     axes[i].legend()
-
-# fig.suptitle('CDF of Colors')
-# save_plot(fig, 'colors_cdf.png')
-
-# 4. Plot and save the CDF of covariance matrix elements
-# fig = plt.figure(figsize=(10, 6))
-# for i, label, color in zip(range(2), ['Covariance Element 1', 'Covariance Element 2'], ['blue', 'orange']):
-#     sorted_data, cdf = calculate_cdf(cov3Ds[:, i])
-#     plt.plot(sorted_data, cdf, color=color, label=label)
-
-# plt.title('CDF of Covariance Matrix Elements')
-# plt.legend()
-# save_plot(fig, 'covariance_cdf.png')
+plt.plot(sorted_opacities, cdf_opacities, color='blue', linewidth=2)
+# plt.title('CDF of Opacities')
+plt.xlabel('Opacity', fontsize=22)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+save_plot(fig, 'result/opacities_cdf.png')
